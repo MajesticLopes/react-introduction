f328fb6b36ad392b4b98c7059f23a89ecc9e0743
Michael Lopes <gh api loopes.09@gmail.com>
2024-01-06 11:15:39 -0500

    React composants et extensions

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Greetings } from "./Greetings"
+
+export function App () {
+  return (
+    <>
+    <input type="text" />
+    <button>Click</button>
+    <Greetings/>
+
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // IMPORTER LES OUTILS REACT
-
+import ReactDOM from "react-dom";
 // IMPORTER NOTRE PREMIER COMPOSANT: APP
-
+import { App } from "./App";
 // CIBLER LA DIV DONT L'ID EST ROOT
+const divRoot = document.getElementById("root");
 
 // CREER UN NOEUD RACINE REACT A PARTIR DE LA DIV ROOT
-
+const reactRoot = ReactDOM.createRoot(divRoot);
 // INJECTER NOTRE PREMIER COMPOSANT LE NOEUD RACINE
+reactRoot.render(<App></App>);
