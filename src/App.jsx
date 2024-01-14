9c0896f0672e1c3f3847c2777b147ad74a7490a0
Michael Lopes <gh api loopes.09@gmail.com>
2024-01-14 17:54:12 -0500

    Mini Projet Picker part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
+import { MenuList } from "./components/MenuList/MenuList";
+import s from "./style.module.css";
 export function App() {
-  return <div>{/*<DisplayDifficulty difficulty="Low" />*/}</div>;
+  const [currentDifficulty, setCurrentDifficulty] = useState("");
+  function updateDifficulty(difficulty) {
+    setCurrentDifficulty(difficulty);
+  }
+  return (
+    <div>
+      <h1 className={s.title}>Select your React difficulty</h1>
+      <div className={s.workspace}>
+        <MenuList
+          onItemClick={updateDifficulty}
+          difficulty={currentDifficulty}
+        />
+        {<DisplayDifficulty difficulty={currentDifficulty} />}
+      </div>
+    </div>
+  );
 }
 
 /*
--- src/components/MenuListItem/MenuListenItem.jsx
+++ src/components/MenuListItem/MenuListenItem.jsx
@@ -1,5 +1,28 @@
 import s from "./style.module.css";
+import { useState } from "react";
 
-function MenuListItem(props) {
-  return <div></div>;
+export function MenuListItem({ onClick, difficulty, isSelected }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  function getBackgroundColor() {
+    if (isHovered) {
+      return "#a5e9ff";
+    } else if (isSelected) {
+      return "#26baea";
+    } else {
+      return "#eff0ef";
+    }
+  }
+  return (
+    <div
+      onClick={() => onClick(difficulty)}
+      style={{ backgroundColor: getBackgroundColor() }}
+      className={s.container}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {" "}
+      Set to : {difficulty}
+    </div>
+  );
 }
