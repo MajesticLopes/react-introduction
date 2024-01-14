d6bc34973eb91cb8191313825bd94adb2d3b8557
Michael Lopes <gh api loopes.09@gmail.com>
2024-01-14 18:32:59 -0500

    Mini Projet Picker part 3 completed

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,28 +1,16 @@
 import { MenuListItem } from "../MenuListItem/MenuListenItem";
 import s from "./style.module.css";
+import { DIFFICULTIES } from "./constant";
 export function MenuList({ difficulty, onItemClick }) {
   return (
     <div className={s.container}>
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Low"
-        isSelected={difficulty === "Low"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Medium"
-        isSelected={difficulty === "Medium"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="High"
-        isSelected={difficulty === "High"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Insane"
-        isSelected={difficulty === "Insane"}
-      />
+      {DIFFICULTIES.map((diff) => (
+        <MenuListItem
+          onClick={onItemClick}
+          difficulty={diff}
+          isSelected={difficulty === diff}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/components/MenuList/constant.js
@@ -0,0 +1,1 @@
+export const DIFFICULTIES = ["Low", "Medium", "high", "Insane"];
